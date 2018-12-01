a7b4030eb60d2109078878e5dca53c36907ed1c1
pauldambra <paul.dambra@gmail.com>
2018-12-01 14:50:47 +0000

    run gradle in debug

--- src/test/kotlin/com/dambra/adventofcode2018/day1/PuzzleInputs.kt
+++ src/test/kotlin/com/dambra/adventofcode2018/day1/PuzzleInputs.kt
@@ -7,7 +7,8 @@ internal class PuzzleInputs {
 
     @Test
     fun `part one`() {
-        val result = javaClass.getResourceAsStream("/day1Part1Input.txt").bufferedReader()
+        val result = javaClass.getResourceAsStream("/day1Part1Input.txt")
+            .bufferedReader()
             .useLines(Sequence<String>::resultingFrequency)
 
         assertThat(result).isEqualTo(533)
@@ -15,7 +16,8 @@ internal class PuzzleInputs {
 
     @Test
     fun `part two`() {
-        val result = javaClass.getResourceAsStream("/day1Part1Input.txt").bufferedReader()
+        val result = javaClass.getResourceAsStream("/day1Part1Input.txt")
+            .bufferedReader()
             .useLines(Sequence<String>::firstRepeatedFrequency)
 
         assertThat(result).isEqualTo(73272)
