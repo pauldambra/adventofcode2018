2db2203b67d61c621b44232d68a00af220a617ed
pauldambra <paul.dambra@gmail.com>
2018-12-08 20:19:14 +0000

    day 8 - part 2 - solution

--- src/test/kotlin/com/dambra/adventofcode2018/day8/ValidatingTheLicenseFile.kt
+++ src/test/kotlin/com/dambra/adventofcode2018/day8/ValidatingTheLicenseFile.kt
@@ -12,7 +12,7 @@ internal class ValidatingTheLicenseFile {
     fun `can sum the metadata in a simplified one node tree`() {
         val exampleInput = "0 3 1 1 2"
 
-        val licenseFile = exampleInput.parse()
+        val licenseFile = LicenseFile.parse(exampleInput)
         val metadataSum = licenseFile.sumMetadata()
         assertThat(metadataSum).isEqualTo(4)
     }
@@ -21,7 +21,7 @@ internal class ValidatingTheLicenseFile {
     fun `can sum metadata in a tree with one empty child node`() {
         val exampleInput = "1 3 0 0 1 1 2"
 
-        val metadataSum = exampleInput.parse().sumMetadata()
+        val metadataSum = LicenseFile.parse(exampleInput).sumMetadata()
         assertThat(metadataSum).isEqualTo(4)
     }
 
@@ -29,7 +29,7 @@ internal class ValidatingTheLicenseFile {
     fun `can sum metadata in a tree with one simple child node`() {
         val exampleInput = "1 3 0 1 1 1 1 2"
 
-        val metadataSum = exampleInput.parse().sumMetadata()
+        val metadataSum = LicenseFile.parse(exampleInput).sumMetadata()
         assertThat(metadataSum).isEqualTo(5)
     }
 
@@ -37,14 +37,28 @@ internal class ValidatingTheLicenseFile {
     fun `Can sum the metadata in the example tree`() {
         val exampleInput = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
 
-        val metadataSum = exampleInput.parse().sumMetadata()
+        val metadataSum = LicenseFile.parse(exampleInput).sumMetadata()
         assertThat(metadataSum).isEqualTo(138)
     }
 
     @Test
+    fun `Can find the root node value in the example tree`() {
+        val exampleInput = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
+
+        val rootNodeValue = LicenseFile.parse(exampleInput).rootNodeValue()
+        assertThat(rootNodeValue).isEqualTo(66)
+    }
+
+    @Test
     fun `Can sum the metadata in the puzzle input tree`() {
-        val metadataSum = puzzleInput.parse().sumMetadata()
+        val metadataSum = LicenseFile.parse(puzzleInput).sumMetadata()
         assertThat(metadataSum).isEqualTo(36627)
+    }
+
+    @Test
+    fun `Can find the root node value in the puzzle input tree`() {
+        val rootNodeValue = LicenseFile.parse(puzzleInput).rootNodeValue()
+        assertThat(rootNodeValue).isEqualTo(16695)
     }
 }
 
@@ -59,7 +73,7 @@ data class LicenseNode(
 
 class LicenseNodeBuilder {
     private var processedChildren: Int = 0
-    private var metadata: MutableList<Int> = mutableListOf()
+    val metadata: MutableList<Int> = mutableListOf()
 
     fun isComplete() = !requiresChildrenCount()
             && !requiresMetadataCount()
@@ -93,91 +107,129 @@ class LicenseNodeBuilder {
         this.childrenCount = childrenCount
     }
 
-    fun build(): LicenseNode {
-        println("children count: $childrenCount")
-        println("metadata count: $metadataCount")
-        println("meta data: $metadata")
-        return LicenseNode(childrenCount!!, metadataCount!!, metadata)
-    }
+    fun build() = LicenseNode(childrenCount!!, metadataCount!!, metadata)
 
 }
 
-private fun String.parse(): LicenseFile {
-    // first char is children count
-    // next character is metadata count
-    // remaining chars are either the children or the metadata
+class LicenseFile(private val rootNode: LicenseNodeBuilder, private val nodes: List<LicenseNode>) {
 
-    val completed = mutableListOf<LicenseNode>()
+    companion object {
 
-    val builderStack = ArrayDeque<LicenseNodeBuilder>()
-    builderStack.push(LicenseNodeBuilder())
+        private val nodeRelationships: MutableMap<LicenseNodeBuilder, MutableSet<LicenseNodeBuilder>> = mutableMapOf()
 
-    val i = this.split(" ")
-        .asSequence()
-        .map { it.toInt() }
-        .iterator()
+        internal fun parse(s: String): LicenseFile {
+            // first char is children count
+            // next character is metadata count
+            // remaining chars are either the children or the metadata
 
-    var currentBuilder: LicenseNodeBuilder? = LicenseNodeBuilder()
+            val completed = mutableListOf<LicenseNode>()
 
-    while (i.hasNext()) {
+            val builderStack = ArrayDeque<LicenseNodeBuilder>()
+            builderStack.push(LicenseNodeBuilder())
 
-        currentBuilder = if (currentBuilder!!.isComplete()) {
-            println("completing node")
-            completeNode(currentBuilder, completed)
-            builderStack.pop()
+            val i = s.split(" ")
+                .asSequence()
+                .map { it.toInt() }
+                .iterator()
+
+            var currentBuilder = LicenseNodeBuilder()
+            val rootNode = currentBuilder
+
+            nodeRelationships.putIfAbsent(currentBuilder, mutableSetOf())
+
+            while (i.hasNext()) {
+
+                currentBuilder = if (currentBuilder.isComplete()) {
+                    completeNode(currentBuilder, completed)
+                    builderStack.pop()
+                } else {
+                    step(currentBuilder, builderStack, i)
+                }
+                nodeRelationships.putIfAbsent(currentBuilder, mutableSetOf())
+            }
+            completeNode(currentBuilder, completed) //final node should be ready now
+            return LicenseFile(rootNode, completed)
+        }
+
+        private fun completeNode(
+            currentBuilder: LicenseNodeBuilder,
+            completed: MutableList<LicenseNode>
+        ) {
+            val licenseNode = currentBuilder.build()
+            completed.add(licenseNode)
+        }
+
+        private fun step(
+            currentBuilder: LicenseNodeBuilder,
+            builderStack: ArrayDeque<LicenseNodeBuilder>,
+            sequence: Iterator<Int>
+        ): LicenseNodeBuilder {
+            if (!sequence.hasNext()) {
+                return currentBuilder
+            }
+
+            when {
+                currentBuilder.requiresChildrenCount() -> {
+                    currentBuilder.addChildrenCount(sequence.next())
+                    return currentBuilder
+                }
+                currentBuilder.requiresMetadataCount() -> {
+                    currentBuilder.addMetadataCount(sequence.next())
+                    return currentBuilder
+                }
+                currentBuilder.requiresChildrenProcessing() -> {
+                    currentBuilder.addChildProcessing()
+                    builderStack.push(currentBuilder)
+
+                    val childBuilder = LicenseNodeBuilder()
+
+                    nodeRelationships[currentBuilder]!!.add(childBuilder)
+
+                    return step(childBuilder, builderStack, sequence)
+                }
+                else -> {
+                    currentBuilder.addMetadata(sequence.next())
+                    return currentBuilder
+                }
+            }
+        }
+    }
+
+    fun rootNodeValue(): Int {
+
+        val x = nodeRelationships
+            .map {
+                it.key to findNodeValue(it.key, it.value)
+            }
+            .toMap()
+
+        val xs = x[rootNode]!!
+
+        return xs.sum()
+    }
+
+    private fun findNodeValue(
+        currentNode: LicenseNodeBuilder,
+        children: MutableSet<LicenseNodeBuilder>
+    ): List<Int> {
+
+        val nodeValue = if (children.isEmpty()) {
+            listOf(currentNode.metadata.sum())
         } else {
-            step(currentBuilder, builderStack, i)
+            currentNode.metadata.map { metadata ->
+                val matchedChild = children.elementAtOrNull(metadata - 1)
+
+                val found = if (matchedChild == null) {
+                    listOf(0)
+                } else {
+                    findNodeValue(matchedChild, nodeRelationships[matchedChild]!!)
+                }
+                found
+            }.flatten()
         }
-    }
-    completeNode(currentBuilder!!, completed) //final node should be ready now
-    return LicenseFile(completed)
-}
-
-private fun completeNode(
-    currentBuilder: LicenseNodeBuilder,
-    completed: MutableList<LicenseNode>
-) {
-    println("found complete builder $currentBuilder")
-    val licenseNode = currentBuilder.build()
-    println("which builds licenseNode with metadata sum ${licenseNode.metadataSum()}")
-    completed.add(licenseNode)
-}
-
-private fun step(
-    currentBuilder: LicenseNodeBuilder,
-    builderStack: ArrayDeque<LicenseNodeBuilder>,
-    sequence: Iterator<Int>
-): LicenseNodeBuilder {
-    if (!sequence.hasNext()) {
-        return currentBuilder
+        return nodeValue
     }
 
-//    println("taking step with builder $currentBuilder")
-
-    when {
-        currentBuilder.requiresChildrenCount() -> {
-            currentBuilder.addChildrenCount(sequence.next())
-            return currentBuilder
-        }
-        currentBuilder.requiresMetadataCount() -> {
-            currentBuilder.addMetadataCount(sequence.next())
-            return currentBuilder
-        }
-        currentBuilder.requiresChildrenProcessing() -> {
-            currentBuilder.addChildProcessing()
-            builderStack.push(currentBuilder)
-
-            val childBuilder = LicenseNodeBuilder()
-            return step(childBuilder, builderStack, sequence)
-        }
-        else -> {
-            currentBuilder.addMetadata(sequence.next())
-            return currentBuilder
-        }
-    }
-}
-
-class LicenseFile(private val nodes: List<LicenseNode>) {
     fun sumMetadata(): Int {
         return nodes.sumBy { it.metadataSum() }
     }
